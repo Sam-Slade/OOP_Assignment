c11b2e563239597e04c49b852aa4c709645d3f9f
MrSpectra <sam@samslade.co.uk>
2020-10-28 14:41:57 +0000

    Added Piece inerface and added comments

--- src/uk/ac/glos/CT5025/S1803267/Game.java
+++ src/uk/ac/glos/CT5025/S1803267/Game.java
@@ -1,6 +1,13 @@
 package uk.ac.glos.CT5025.S1803267;
 
 public class Game {
+  /* Game class
+   * Author: Sam Slade
+   * ID: c01
+   *
+   * Desc: The Game class is the entry point and game
+   * state manager of the whole program.
+   */
   public static void main(String[] args) {
     System.out.println("test");
   }
---
+++ src/uk/ac/glos/CT5025/S1803267/pieces/Piece.java
@@ -0,0 +1,22 @@
+package uk.ac.glos.CT5025.S1803267.pieces;
+
+public interface Piece {
+  /* Piece interface
+   * Author: Sam Slade
+   * ID: i01
+   *
+   * Desc: This interface is the basis of all the pieces
+   * int the chess game. Each piece should be able to have
+   * a colour set and be able to check if a move is valid.
+   */
+
+  // The piece checks to see if a move is valid and returns a boolean
+  public boolean checkValidMove(Board board, String move);
+
+  // Colours are stored as 0 for black and 1 for white
+  public void setColour(int colour);
+  public int getColour();
+
+  // Add to the move counter stored for a piece
+  public void addMove();
+}
